8e216fa6b48d9ef737f2855955c68422813c2f29
Nojj02 <nojj02@gmail.com>
2017-10-08 00:39:38 +0800

    Added in initial projects, one test

---
+++ src/Cornucopia.Domain.Tests/Models/FinancialNeedsAnalysisTests.cs
@@ -0,0 +1,24 @@
+﻿using Cornucopia.Domain.Models;
+using Xunit;
+
+namespace Cornucopia.Domain.Tests.Models
+{
+    public class FinancialNeedsAnalysisTests
+    {
+        public class NewTests : FinancialNeedsAnalysisTests
+        {
+            [Fact]
+            public void TargetRetirementFundIsCurrentLivingExpensesMultipliedByTenYears()
+            {
+                var financialNeedsAnalysis =
+                    new FinancialNeedsAnalysis(
+                        clientName: "Juan De La Cruz",
+                        age: 25,
+                        targetRetirementAge: 25,
+                        currentLivingExpenses: 10500);
+
+                Assert.Equal(105000, financialNeedsAnalysis.TargetRetirementFund);
+            }
+        }
+    }
+}
---
+++ src/Cornucopia.Domain/Models/FinancialNeedsAnalysis.cs
@@ -0,0 +1,29 @@
+﻿namespace Cornucopia.Domain.Models
+{
+    public class FinancialNeedsAnalysis
+    {
+        public FinancialNeedsAnalysis(
+            string clientName,
+            int age,
+            int targetRetirementAge,
+            int currentLivingExpenses)
+        {
+            ClientName = clientName;
+            Age = age;
+            TargetRetirementAge = targetRetirementAge;
+            CurrentLivingExpenses = currentLivingExpenses;
+
+            TargetRetirementFund = CurrentLivingExpenses * 10;
+        }
+
+        public string ClientName { get; }
+
+        public int Age { get; }
+
+        public int TargetRetirementAge { get; }
+
+        public decimal CurrentLivingExpenses { get; }
+
+        public decimal TargetRetirementFund { get; }
+    }
+}
